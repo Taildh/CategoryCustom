c05b0300f0be917ba884e959a534d41377f4fdef
taildh <ledinhhuutaii@gmail.com>
2019-11-25 14:36:33 +0700

    update get pr,cate

--- Block/Cate.php
+++ Block/Cate.php
@@ -1,21 +1,50 @@
 <?php
-
 namespace Dtn\CategoryCustom\Block;
 
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\Data\CollectionFactory;
-use Magento\Framework\View\Result\PageFactory;
-use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Catalog\Model\ProductFactory;
 
 class Cate extends \Magento\Framework\View\Element\Template
 {
-
+    protected $_categoryFactory;
 
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        array $data = []
     )
     {
-        parent::__construct($context);
+        $this->_categoryFactory = $categoryFactory;
+        parent::__construct($context, $data);
     }
 
+    public function getCategory($categoryId)
+    {
+        $category = $this->_categoryFactory->create();
+        $category->load($categoryId);
+        return $category;
+    }
+    public function getChildCategories($categoryId)
+    {
+
+        $_category = $this->_categoryFactory->create();
+
+        $category = $_category->load($categoryId);
+
+        //Get category collection
+        $collection = $category->getCollection()
+            ->addIsActiveFilter()
+            ->addOrderField('name')
+            ->addIdFilter($category->getChildren());
+        return $collection;
+    }
+
+    public function getCategoryProducts($categoryId)
+    {
+        $products = $this->getCategory($categoryId)->getProductCollection()
+                    ->addAttributeToSelect('*');
+        return $products;
+    }
 }
